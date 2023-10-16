1222d74f22da3eac6010d954fe94fa5897483d0e
NickNovic <thenicknovicov@gmail.com>
2023-10-16 19:15:09 +0300

    base presetup

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 using EndavaTechCourseBankApp.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 namespace EndavaTechCourseBankApp.Client
 {
@@ -13,6 +14,8 @@ namespace EndavaTechCourseBankApp.Client
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
