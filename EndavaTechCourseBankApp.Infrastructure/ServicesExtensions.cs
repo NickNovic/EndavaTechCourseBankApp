58b3907e90f4554fcac0c8c507e39dea0c548527
NickNovic <thenicknovicov@gmail.com>
2023-10-24 16:36:19 +0300

    Infrastructure checked

--- EndavaTechCourseBankApp.Infrastructure/ServicesExtensions.cs
+++ EndavaTechCourseBankApp.Infrastructure/ServicesExtensions.cs
@@ -12,19 +12,11 @@ public static class ServicesExtensions
 {
 
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
-
     {
-
         services.AddDbContext<ApplicationDbContext>(options =>
-
             options.UseSqlServer(
-
                 configuration.GetConnectionString("DefaultConnection"),
-
                 b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
-
         return services;
-
     }
-
 }
